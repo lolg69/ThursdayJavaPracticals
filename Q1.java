4fdd2692b9d0b29ec0e9f6b92d884008c889d8e7
lolg69 <69385870+lolg69@users.noreply.github.com>
2022-07-07 12:30:18 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,8 @@
+private String notebook	= idol
+private float procSpeedGHz = 2.66
+private byte RAMGB = 8
+private short diskSpaceGB = 500 
+private byte monitorLengthInch = 15
+private float priceDollar = 1760.55
+private char servicePlan = C
+private boolean extendedWarranty = False 						
