e7082f079225403e7b79c227cb928dd697955e56
lolg69 <69385870+lolg69@users.noreply.github.com>
2022-07-08 10:30:16 +0530

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -1,13 +1,13 @@
 
 class Notebook{
   public static void main(String[] args){
-    String notebook	= idol
+    String notebook	= "idol"
     float procSpeedGHz = 2.66
     byte RAMGB = 8
     short diskSpaceGB = 500 
     byte monitorLengthInch = 15
     float priceDollar = 1760.55
-    char servicePlan = C
+    char servicePlan = "C"
     boolean extendedWarranty = False
     }
 }
